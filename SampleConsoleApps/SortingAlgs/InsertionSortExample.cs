6b29a16348e7ab3f69a545e99634449e922a533d
Gabriele Tronchin <gabriele.tronchin@reti.it>
2023-11-16 13:07:13 +0100

    clean

--- SampleConsoleApps/SortingAlgs/InsertionSortExample.cs
+++ SampleConsoleApps/SortingAlgs/InsertionSortExample.cs
@@ -1,57 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SampleConsoleApps.Algs
+﻿namespace SampleConsoleApps.Algs
 {
     public static class InsertionSortExample
     {
-       public static void Main()
-    {
-        int[] array = { 64, 34, 25, 12, 22, 11, 90 };
-
-        Console.WriteLine("Original array:");
-        PrintArray(array);
-
-        SelectionSort(array);
-
-        Console.WriteLine("\nSorted array:");
-        PrintArray(array);
-    }
-
-    static void SelectionSort(int[] arr)
-    {
-        int n = arr.Length;
-
-        for (int i = 0; i < n - 1; i++)
+        public static void Main()
         {
-            // Find the minimum element in the unsorted part of the array
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++)
+            int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+
+            Console.WriteLine("Original array:");
+            PrintArray(array);
+
+            SelectionSort(array);
+
+            Console.WriteLine("\nSorted array:");
+            PrintArray(array);
+        }
+
+        static void SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
             {
-                if (arr[j] < arr[minIndex])
+                // Find the minimum element in the unsorted part of the array
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
                 {
-                    minIndex = j;
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
                 }
+
+                // Swap the found minimum element with the first element
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
             }
-
-            // Swap the found minimum element with the first element
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
         }
-    }
 
-    static void PrintArray(int[] arr)
-    {
-        foreach (var item in arr)
+        static void PrintArray(int[] arr)
         {
-            Console.Write(item + " ");
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
         }
-        Console.WriteLine();
-    }
     }
 
 }
