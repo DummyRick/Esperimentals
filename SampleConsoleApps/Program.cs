b887fca2ca1f1ad7b67053f9f3a8133a840e62df
Gabriele Tronchin <gabriele.tronchin@reti.it>
2023-11-16 13:14:32 +0100

    add samples

--- SampleConsoleApps/Program.cs
+++ SampleConsoleApps/Program.cs
@@ -3,31 +3,51 @@ using SampleConsoleApps.Algs;
 using SampleConsoleApps.Graph;
 using SampleConsoleApps.SearchingAlgs;
 
+Console.WriteLine($"******************************************");
 Console.WriteLine(nameof(BubbleSortExample));
 BubbleSortExample.Main();
+Console.WriteLine("");
 
 
+Console.WriteLine($"******************************************");
 Console.WriteLine(nameof(InsertionSortExample));
 InsertionSortExample.Main();
+Console.WriteLine("");
 
-
-
+Console.WriteLine($"******************************************");
 Console.WriteLine(nameof(MergeSortExample));
 MergeSortExample.Main();
+Console.WriteLine("");
 
-
+Console.WriteLine($"******************************************");
 Console.WriteLine(nameof(QuickSortExample));
 QuickSortExample.Main();
+Console.WriteLine("");
 
+
+Console.WriteLine($"******************************************");
 Console.WriteLine(nameof(SelectionSortExample));
 SelectionSortExample.Main();
+Console.WriteLine("");
 
+Console.WriteLine($"******************************************");
 Console.WriteLine(nameof(BinarySearchExample));
 BinarySearchExample.Main();
+Console.WriteLine("");
 
+
+Console.WriteLine($"******************************************");
 Console.WriteLine(nameof(BinarySearchExample));
 BinarySearchExample.Main();
+Console.WriteLine("");
 
-
+Console.WriteLine($"******************************************");
 Console.WriteLine(nameof(BFSExample));
 BFSExample.Main();
+Console.WriteLine("");
+
+
+Console.WriteLine($"******************************************");
+Console.WriteLine(nameof(DFSExample));
+DFSExample.Main();
+Console.WriteLine("");
--- SampleConsoleApps/SearchingAlgs/BinarySearchExample.cs
+++ SampleConsoleApps/SearchingAlgs/BinarySearchExample.cs
@@ -61,3 +61,4 @@
             Console.WriteLine();
         }
     }
+}
